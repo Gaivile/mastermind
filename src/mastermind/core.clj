3f4ae33a514dde58d148585771e891f2dc894164
Gaivile <gaivile.vezeviciute@healthunlocked.com>
2017-05-30 21:34:27 +0100

    :hamburger: Simplified working version

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -1,6 +1,18 @@
 (ns mastermind.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def secret
+  [(rand-int 10) (rand-int 10) (rand-int 10) (rand-int 10)])
+
+(defn guess
+  [guess-numbers]
+  (let [answers {:right-place-and-number
+                 (->> (interleave guess-numbers secret)
+                      (partition 2 )
+                      (filter #(= (first %) (second %)) )
+                      (count))
+                 :right-number (-> (filter #(.contains secret %) guess-numbers)
+                                   (count))}]
+    {:right-place-and-number (:right-place-and-number answers)
+     :right-number (- (:right-number answers) (:right-place answers))}))
+
+;; (guess [9 1 4 6])
